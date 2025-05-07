feec8acf02ea5be4fbc029035f2aa4287de6f524
Sarah <sarahelhefnawy0@gmail.com>
2025-05-07 23:38:59 +0300

    Design improvements

---
+++ js/main.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.typed', {
+    strings: ["Photographer", "Developer", "Freelancer", "Photographer"],
+    typeSpeed: 50,
+    backSpeed: 60,
+    backDelay: 500,
+    startDelay: 1000,
+    loop: true,
+});
