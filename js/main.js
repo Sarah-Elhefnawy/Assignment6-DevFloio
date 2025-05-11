651b14b93dfbc3b44e1bf62b10d692b7db43137a
Sarah <sarahelhefnawy0@gmail.com>
2025-05-11 17:12:24 +0300

    Anchor + Navbar Edits

--- js/main.js
+++ js/main.js
@@ -1,3 +1,29 @@
+const navModal = document.getElementById("navbarModal");
+const navbar = document.querySelector(".navbar");
+var btns = document.querySelectorAll(".nav-link, .dropdown-item");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        navModal.style.display = "none";
+        const backdrop = document.querySelector(".modal-backdrop");
+        if (backdrop) {
+            backdrop.remove();
+        }
+        document.body.style.overflow = "auto";
+        document.body.style.paddingRight = "0";
+        navbar.style.paddingRight = "0";
+    });
+});
+
+window.addEventListener("scroll", () => {
+  const button = document.querySelector(".scroll-to-top");
+  if (window.scrollY) { 
+    button.classList.add("show");
+  } else {
+    button.classList.remove("show");
+  }
+});
+
 var typed = new Typed('.typed', {
     strings: ["Designer", "Developer", "Freelancer", "Photographer"],
     typeSpeed: 50,
